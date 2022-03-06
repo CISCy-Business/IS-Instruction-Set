c9260bf916beb69e1b10284092b8fea9cc349e65
PrestonSC <prestoncunningham01@gmail.com>
2022-03-06 06:14:17 -0500

    more changes

---
+++ InstructionSetProject.Backend/StaticPipeline/StaticPipelineStatistics.cs
@@ -0,0 +1,56 @@
+﻿using InstructionSetProject.Backend.InstructionTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstructionSetProject.Backend.StaticPipeline
+{
+    public class StaticPipelineStatistics
+    {
+        public int FlushCount { get; set; } = 0;
+        public int ClockTicks { get; set; } = 0;
+
+        public int R2InstructionCount { get; set; } = 0;
+        public int R3InstructionCount { get; set; } = 0;
+        public int RmInstructionCount { get; set; } = 0;
+        public int RsInstructionCount { get; set; } = 0;
+        public int F2InstructionCount { get; set; } = 0;
+        public int F3InstructionCount { get; set; } = 0;
+        public int FmInstructionCount { get; set; } = 0;
+
+
+        public void StatInstructionType (IInstruction instr)
+        {
+            if (instr is R2Instruction)
+            {
+                R2InstructionCount++;
+            }
+            if (instr is R3Instruction)
+            {
+                R3InstructionCount++;
+            }
+            if (instr is RmInstruction)
+            {
+                RmInstructionCount++;
+            }
+            if (instr is RsInstruction)
+            {
+                RsInstructionCount++;
+            }
+            if (instr is F2Instruction)
+            {
+                F2InstructionCount++;
+            }
+            if (instr is F3Instruction)
+            {
+                F3InstructionCount++;
+            }
+            if (instr is FmInstruction)
+            {
+                FmInstructionCount++;
+            }
+        }
+    }
+}
